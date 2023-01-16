5c4e693c3667badd163e174a5447363b7a03af9b
Sam Gardner <stgardner4@wx4stg.com>
2023-01-16 22:54:03 +0000

    initial commit

---
+++ backup_script.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from os import path, system, listdir, remove
+from datetime import datetime as dt
+from pathlib import Path
+
+if __name__ == "__main__":
+    backupsPath = "/backup/"
+    backupsPathContents = sorted(listdir(backupsPath))
+    targetFolderName = dt.utcnow().strftime("%Y-%m-%d_%H:%M:%S")
+    Path(path.join(backupsPath, targetFolderName)).mkdir(parents=True, exist_ok=True)
+    Path(path.join(backupsPath, "gdrivelink")).mkdir(parents=True, exist_ok=True)
+    if len(backupsPathContents) > 0:
+        oldestBackup = backupsPathContents[0]
+        print("rsync -vaxcH --progress --exclude=/backup/ --compare-dest=/backup/"+oldestBackup+"/. /. /backup/"+targetFolderName+"/")
+        system("rsync -vaxcH --progress --exclude=/backup/ --compare-dest=/backup/"+oldestBackup+"/. /. /backup/"+targetFolderName+"/")
+        system("diff -rq / /backup/2023-01-16_22:20:05 | grep \"Only in /backup/\" >> /backup/"+targetFolderName+"/delete.txt")
+        with open("/backup/"+targetFolderName+"/delete.txt", "r") as f:
+            deletions = f.read()
+        deletions = deletions.replace("Only in /backup/"+targetFolderName, "").replace(": ", "")
+        remove("/backup/"+targetFolderName+"/delete.txt")
+        with open("/backup/"+targetFolderName+"/delete.txt", "w") as f:
+            f.write(deletions)
+    else:
+        system("rsync -vaxcH --progress --exclude=/backup/ /. /backup/"+targetFolderName+"/")
+    system("tar -czvf /backup/gdrivelink/"+targetFolderName+".tar.gz /backup/"+targetFolderName)
+    system("chown -R stgardner4:stgardner4 /backup/gdrivelink/")
+    system("sudo -u stgardner4 rclone sync /backup/gdrivelink/ sam_wx4stg_gdrive:backups --bwlimit=8.5M --progress")
